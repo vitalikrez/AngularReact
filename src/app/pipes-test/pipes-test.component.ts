84d16fe2b6ea0f34c692dc2cc180d86213462225
Vitaliy Rez <vitalik.rez@gmail.com>
2022-09-14 21:04:30 +0300

    commit

--- src/app/pipes-test/pipes-test.component.ts
+++ src/app/pipes-test/pipes-test.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import  localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localeDe,'de');
 @Component({
   selector: 'app-pipes-test',
   template: `
@@ -13,6 +16,21 @@ import { Component, OnInit } from '@angular/core';
   <div>{{str2 | slice:-8}}</div>
   <div>{{pi | number:'3.5-5'}}</div>
   <div>{{pi | number:'2.1-2'}}</div>
+  <div>{{pi | number:'2.1-2':'de'}}</div>
+
+    <div>{{money | currency:'USD'}}</div>
+    <div>{{money | currency:'UAH'}}</div>
+
+    <div>{{money | currency:'UAH': 'symbol-narrow':'1.1-2'}}</div>
+    <div>{{money | currency:'UAH': 'symbol-narrow'}}</div>
+
+    <div>{{a | percent}}</div>
+    <div>{{b | percent}}</div>
+    <div>{{c | percent:'3.3-5'}}</div>
+
+    <div>Form object{{dataObject | date</div>
+    <div>Form string{{dataString | date</div>
+    <div>Form number{{dataObject | date</div>
 
 
   
@@ -25,6 +43,14 @@ export class PipesTestComponent implements OnInit {
   str2 = 'London is the capital of Great Britain!'
   num = 0.14;
   pi=3.14;
+  money=100;
+  a=0.2;
+  b=0.4;
+  c=0.5
+
+  dataObject: Date = new Date(2022,9.14);
+  dataString = '14092022';
+
   constructor() { }
 
   ngOnInit(): void {
