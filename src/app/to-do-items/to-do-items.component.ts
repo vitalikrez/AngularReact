defa960527d9edf532d45624a2c8e76274105d60
Vitaliy Rez <vitalik.rez@gmail.com>
2022-09-15 21:17:01 +0300

    End of lesson 3

--- src/app/to-do-items/to-do-items.component.ts
+++ src/app/to-do-items/to-do-items.component.ts
@@ -6,15 +6,33 @@ import { ITEMS } from '../mock-todo-item';
   selector: 'app-to-do-items',
   templateUrl: './to-do-items.component.html',
   styleUrls: ['./to-do-items.component.css']
+
 })
 export class ToDoItemsComponent implements OnInit {
 
   items = ITEMS;
+
 toDoItem: ToDoItem ={
   id: 1,
   name: "Learn Angular",
   isComplete: false
 }
+
+selectedItem!:ToDoItem;
+visibility: boolean = true;
+
+onSelect(item:ToDoItem): void{
+  this.selectedItem = item;
+
+  if (item.isComplete==false)
+        item.isComplete=true
+ else   item.isComplete=false;
+ 
+}
+
+car='Volvo';
+
+
 
   constructor() { }
 
